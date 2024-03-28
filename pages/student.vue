f5f581a1f0c6bd27d458d033720aa2f03d12b884
Petro Burchak <burchakpetro@gmail.com>
2024-03-29 00:23:08 +0200

    Feat: add products to "Student List". Created Pagination & Filtration in pages.

--- pages/student.vue
+++ pages/student.vue
@@ -1,116 +1,101 @@
-<script setup lang="ts">
-const people = [{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-}, {
-  id: 7,
-  name: 'Emily Selman',
-  title: 'VP, User Experience',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 8,
-  name: 'Kristin Watson',
-  title: 'VP, Human Resources',
-  email: '',
-  role: 'Member'
-}, {
-  id: 9,
-  name: 'Emma Watson',
-  title: 'Front-end Developer',
-  email: '',
-  role: 'Member'
-}, {
-  id: 10,
-  name: 'John Doe',
-  title: 'Designer',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 11,
-  name: 'Jane Doe',
-  title: 'Director of Product',
-  email: '',
-  role: 'Member'
-}, {
-  id: 12,
-  name: 'John Smith',
-  title: 'Copywriter',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 13,
-  name: 'Jane Smith',
-  title: 'Senior Designer',
-  email: '',
-  role: 'Owner'
-}]
+<script setup>
+const columns = [
+  {
+    key: 'title',
+    label: 'Title'
+  },
+  {
+    key: 'description',
+    label: 'Description'
+  },
+  {
+    key: 'price',
+    label: 'Price'
+  },
+  {
+    key: 'rating',
+    label: 'Rating'
+  },
+  {
+    key: 'brand',
+    label: 'Brand'
+  },
+  {
+    key: 'category',
+    label: 'Category'
+  },
+  {
+    key: 'thumbnail',
+    label: 'Thumbnail'
+  }
+]
 
-const q = ref('')
+const { data } = await useFetch('https://dummyjson.com/products');
+const people = data.value.products;
 
-const filteredRows = computed(() => {
+const q = ref('');
+const page = ref(1);
+const pageCount = 5;
+
+
+const filtredData =() => {
   if (!q.value) {
-    return people
+    return people;
   }
 
   return people.filter((person) => {
     return Object.values(person).some((value) => {
-      return String(value).toLowerCase().includes(q.value.toLowerCase())
-    })
-  })
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
+    });
+  });
+};
+
+const filtredRows =  computed(() => {
+  return filtredData();
 })
-
-const page = ref(1)
-const pageCount = 5
-
 const rows = computed(() => {
-  return filteredRows.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
+  if (filtredData().length <= 5 ) {
+    page.value = 1;
+  }
+  const slice = filtredData()
+      .slice((page.value - 1) * pageCount, (page.value) * pageCount);
+  console.log(slice)
+  return slice });
 </script>
-
 <template>
+  <title>Student list</title>
+
   <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
     <UInput v-model="q" placeholder="Filter people..." />
   </div>
-  <div>
-    <UTable :rows="rows" />
 
-    <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-      <UPagination v-model="page" :page-count="pageCount" :total="people.length" />
+  <div>
+    <UTable :rows="rows" :columns="columns">
+      <template #thumbnail-data="{row}">
+        <img :src="row.thumbnail" alt="" class="w-[100px] h-[100px]">
+      </template>
+      <template #description-data="{row}">
+        <div class="text-wrap">{{ row.description }}</div>
+      </template>
+      <template #price-data="{row}">
+        <div>{{ row.price }}</div>
+      </template>
+      <template #rating-data="{row}">
+        <div :class="{ 'bg-red-500/40': row.rating < 4.5, 'bg-green-500/40': row.rating >= 4.5 }" class="px-3 py-2">
+          {{ row.rating }}
+        </div>
+      </template>
+      <template #brand-data="{row}">
+        <div>{{ row.brand }}</div>
+      </template>
+      <template #category-data="{row}">
+        <div>{{ row.category }}</div>
+      </template>
+    </UTable>
+    <div class="flex justify-center px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+      <UPagination v-model="page" :page-count="pageCount" :total="filtredRows.length" />
     </div>
   </div>
 </template>
+
 
